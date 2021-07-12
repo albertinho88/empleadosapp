90527267b8b6c591bc27bee3a5572e807f792b2d
alberto <alberto@bserver>
2021-07-11 22:30:52 -0500

    Creación proyecto con Spring Boot

---
+++ src/main/java/com/example/empleadosapp/EmpleadosappApplication.java
@@ -0,0 +1,13 @@
+package com.example.empleadosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/empleadosapp/EmpleadosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empleadosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
